498182faf8606f2bd61fabeec883c55bd2ecfdbb
sudmed <pashkov.dmitriy@gmail.com>
2022-04-03 23:59:22 +0300

    Create hw@Bash_03_scripts.sh

---
+++ hw@Bash_03_scripts.sh
@@ -0,0 +1,19 @@
+#Homework@03_scripts
+#! /bin/bash
+
+echo "Creating folder"
+mkdir Name_Surname
+ls
+echo "Creating files"
+touch Name
+touch Surname
+ls -d */ | cut -f1 -d'/'
+
+: '
+./script.sh
+Creating folder
+mkdir: cannot create directory ‘Name_Surname’: File exists
+Name  Name_Surname  script.sh  Surname
+Creating files
+Name_Surname
+'
