44fde472ae264c72bf7268bb3f5eb67741a054fa
sudmed <pashkov.dmitriy@gmail.com>
2022-04-04 21:02:19 +0300

    Create hw@Bash_04_variables.sh

---
+++ @Bash_04_variables/hw@Bash_04_variables.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#homework@Bash_04_variables
+
+
+NAME=Name_Surname
+source vars
+mkdir $NAME
+#cd $NAME
+touch ./$NAME/$FILE
+ls ./
+ls ./$NAME
+
+
+: '
+./script.sh 
+Name_Surname  script.sh  vars
+new_file
+'
